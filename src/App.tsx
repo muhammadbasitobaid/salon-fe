ab8db5aeac73d7a78b601fb2ba834d302b3745e6
muhammadbasitobaid <muhammadbasitobaid@gmail.com>
2023-10-23 18:57:36 +0500

    added side menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,33 @@
-import { useDispatch, useSelector } from "react-redux";
-import "./App.css";
+// import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components/macro";
-import { decrement, increment } from "./features/counter/counterSlice";
+// import { decrement, increment } from "./features/counter/counterSlice";
+import { Outlet } from "react-router-dom";
+import SideBar from "./SideBar";
+import { routes } from "./routes";
 
 function App() {
-  const counterState = useSelector((state: any) => state.counter);
-  const { value: count } = counterState;
-  const dispatch = useDispatch();
+  // const counterState = useSelector((state: any) => state.counter);
+  // const { value: count } = counterState;
+  // const dispatch = useDispatch();
+
+  let [appMenuList] = routes;
+  const menuItems = appMenuList?.children?.map((item) => ({
+    id: item?.id,
+    text: item?.text,
+    path: item?.path,
+  }));
 
   return (
     <Container className="App">
-      <div>
-        <button
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          +
-        </button>
-        <span>{count}</span>
-        <button
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          -
-        </button>
-      </div>
+      <SideBar menuItems={menuItems} />
+      <Outlet />
     </Container>
   );
 }
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+`;
 
 export default App;
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,33 @@
 import App from "./App";
 import Login from "./pages/Login";
+import { v4 as uuid } from "uuid";
+import ContactUs from "./ContactUs";
 
 export const routes = [
   {
     path: "/",
     exact: true,
     element: <App />,
+    children: [
+      {
+        id: uuid(),
+        path: "dashboard",
+        text: "Dashboard",
+        // element: <Contact />,
+      },
+      {
+        id: uuid(),
+        path: "booking",
+        text: "Book a visit",
+        // element: <Contact />,
+      },
+      {
+        id: uuid(),
+        path: "user-settings",
+        text: "User Settings",
+        element: <ContactUs />,
+      },
+    ],
   },
   {
     path: "/login",
