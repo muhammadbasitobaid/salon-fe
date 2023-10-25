db0d60c449499e7431c2177ad0a2621e44368fc5
muhammadbasitobaid <muhammadbasitobaid@gmail.com>
2023-10-25 14:42:22 +0500

    bug fix eslint useless escape in signup file for special char validation in password feild

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -20,7 +20,7 @@ const SignupSchema = Yup.object().shape({
     .min(8, "Password must be at least 8 characters")
     .required("Password is required")
     .matches(
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/,
       "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character"
     ),
   confirmPassword: Yup.string()
